52ee30c3838c1651bf1f946a72767c1e35cea78c
Andrrr <andresin@gmail.com>
2018-02-16 12:39:11 -0300

    Optimización de descargas (PDF/HTML), refactor para que sea escalable

--- modules/descargas/controller/descargas.ts
+++ modules/descargas/controller/descargas.ts
@@ -6,87 +6,121 @@ import { Auth } from '../../../auth/auth.class';
 
 export class Documento {
 
-    private static locale = 'es-ES';
-    private static timeZone = 'America/Argentina/Buenos_Aires';
+    /**
+     * Opciones de Geolocalización
+     */
+    private locale = 'es-ES';
+    private timeZone = 'America/Argentina/Buenos_Aires';
 
     /**
      * Opciones default de PDF rendering
      */
-    private static options = {
-        format: 'A4',
-        border: {
-            // default is 0, units: mm, cm, in, px
-            top: '0.5cm',
-            right: '0.5cm',
-            bottom: '1.5cm',
-            left: '1.5cm'
-        },
-        header: {
-            height: '2.5cm',
-        },
-        footer: {
-            height: '10mm',
-            contents: {
+    private options = {};
+
+    constructor(options) {
+        this.options = {
+            format: 'A4',
+            border: {
+                // default is 0, units: mm, cm, in, px
+                top: '0.5cm',
+                right: '0.5cm',
+                bottom: '1.5cm',
+                left: '1.5cm'
+            },
+            header: {
+                height: '2.5cm',
+            },
+            footer: {
+                height: '10mm',
+                contents: {
+                }
+            },
+            settings: {
+                resourceTimeout: 30
+            },
+        };
+    }
+
+    private async cargarAssets(modulo) {
+        return new Promise((resolve, reject) => {
+            // Se cargan logos
+            let logoAndes: Buffer = fs.readFileSync('./templates/andes/logo-andes.png');
+            let logotipoAndes: Buffer = fs.readFileSync('./templates/andes/logotipo-andes-blue.png');
+            let logoPDP: Buffer = fs.readFileSync('./templates/andes/logo-pdp.png');
+
+            let header = fs.readFileSync(`./templates/${modulo}/header.html`).toString();
+            let footer = fs.readFileSync(`./templates/${modulo}/footer.html`).toString();
+
+            if (!logoAndes || !logotipoAndes || !logoPDP || !header || !footer) {
+                reject('Error al cargar archivo.');
             }
-        },
-        settings: {
-            resourceTimeout: 30
-        },
-    };
+
+            resolve({ header: header, footer: footer, logos: { logoAndes: logoAndes, logotipoAndes: logotipoAndes, logoPDP: logoPDP } });
+        });
+    }
 
     /**
      *
      * @param req ExpressJS request
      * TODO: Extender
      */
-    private static generarHTML(req) {
+    private async generarHTML(req) {
+        return new Promise(async (resolve, reject) => {
 
-        // Se genera HTML para ser transformado en PDF
-        let html = Buffer.from(req.body.html, 'base64').toString();
+            // Se genera HTML para ser transformado en PDF
+            let html = await Buffer.from(req.body.html, 'base64').toString();
 
-        // Se agregan los estilos CSS
-        html += this.generarCSS();
+            // Se agregan los estilos CSS
+            html += await this.generarCSS(req.body.modulo);
 
-        // Se cargan logos
-        let logoAndes = fs.readFileSync('./templates/andes/logo-andes.png');
-        let logotipoAndes = fs.readFileSync('./templates/andes/logotipo-andes-blue.png');
-        let logoPDP = fs.readFileSync('./templates/andes/logo-pdp.png');
+            let assets: any = await this.cargarAssets(req.body.modulo);
 
-        // Se reemplazan ciertos <!--placeholders--> por logos de ANDES y Dirección de Protección de Datos Personales
-        html = html.replace('<!--logoAndes-->', `<img src="data:image/png;base64,${logoAndes.toString('base64')}" style="float: left;">`);
-        html = html.replace('<!--logotipoAndes-->', `<img src="data:image/png;base64,${logotipoAndes.toString('base64')}" style="width: 80px; margin-right: 10px;">`);
+            // Se reemplazan ciertos <!--placeholders--> por diferentes assets y datos
+            let header = assets.header.replace('<!--placeholderLogoAndes-->', `<img src="data:image/png;base64,${assets.logos.logoAndes.toString('base64')}" style="float: left;">`);
+            header = header.replace('<!--placeholderLogotipoAndes-->', `<img src="data:image/png;base64,${assets.logos.logotipoAndes.toString('base64')}" style="width: 80px; margin-right: 10px;">`);
+            header = header.replace('<!--placeholderOrganizacion-->', Auth.getOrganization(req, 'nombre'));
 
-        html += `<footer id="pageFooter" style="background-color: rgba(0,0,0,0.1); display: inline-block; font-size: 8px; margin-bottom: 15px; padding: 5px;">
-        <img src="data:image/png;base64,${logoPDP.toString('base64')}" style="display: inline-block; width: 100px; float: right;">
-        <div style="display: inline-block; float: left; width: 400px; margin-right: 10px; text-align: justify;">
-            El contenido de este informe ha sido validado digitalmente siguiendo los estándares de calidad y seguridad requeridos. El   Hospital Provincial Neuquén es responsable Inscripto en el Registro Nacional de Protección de Datos Personales bajo el N° de Registro 100000182, según lo requiere la Ley N° 25.326 (art. 3° y 21 inciso 1)
-            ${JSON.stringify(Auth.getUserName(req))} - ${new Date().toLocaleString('locale', { timeZone: this.timeZone })} hs
-        </div>
-    </footer>`;
+            html += header;
 
-        return html;
+            let footer = assets.footer.replace('<!--placeholderLogoPDP-->', `<img src="data:image/png;base64,${assets.logos.logoPDP.toString('base64')}" style="width: 100px; float: right;">`);
+            footer = footer.replace('<!--placeholderSessionData-->', `${JSON.stringify(Auth.getUserName(req))} - ${new Date().toLocaleString('locale', { timeZone: this.timeZone })}`);
+
+            html += footer;
+
+            resolve(html);
+
+        });
+
     }
 
     /**
      * Genera CSS de RUP
      * TODO: Extender
      */
-    private static generarCSS() {
+    private async generarCSS(modulo) {
+        return new Promise((resolve, reject) => {
+            fs.stat(`./templates/${modulo}/styles.scss`, (err, stats) => {
 
-        // Se agregan los estilos
-        let css = '<style>\n\n';
+                if (err) {
+                    reject(err);
+                }
 
-        // SCSS => CSS
-        css += scss.renderSync({
-            file: './templates/rup/prestacionValidacion-print.scss',
-            includePaths: [
-                './templates/rup/'
-            ]
-        }).css;
+                // Se agregan los estilos
+                let css = '<style>\n\n';
 
-        css += '</style>';
+                // SCSS => CSS
+                css += scss.renderSync({
+                    file: `./templates/${modulo}/styles.scss`,
+                    includePaths: [
+                        './templates/${modulo}/'
+                    ]
+                }).css;
 
-        return css;
+                css += '</style>';
+                resolve(css);
+
+            });
+        });
     }
 
     /**
@@ -95,30 +129,46 @@ export class Documento {
      * @param res ExpressJS response
      * @param next ExpressJS next
      * @param options html-pdf/PhantonJS rendering options
+     * TODO: extender
      */
-    static generarPDF(req, res, next, options = null) {
+    private async generar(req, next, options = null) {
+        return new Promise(async (resolve, reject) => {
 
-        // PhantomJS PDF rendering options
-        // https://www.npmjs.com/package/html-pdf
-        // http://phantomjs.org/api/webpage/property/paper-size.html
-        if (options) {
-            this.options = options;
-        }
-
-        let html = this.generarHTML(req);
-
-        pdf.create(html, this.options).toFile((err2, file) => {
-
-            if (err2) {
-                return next(err2);
+            // PhantomJS PDF rendering options
+            // https://www.npmjs.com/package/html-pdf
+            // http://phantomjs.org/api/webpage/property/paper-size.html
+            if (options) {
+                this.options = options;
             }
 
-            res.download(file.filename, (err3) => {
-                if (err2) {
-                    next(err3);
-                } else {
-                    next();
+            let html = await this.generarHTML(req);
+
+            // Tipo de archivo a descargar (html|pdf)
+            switch (req.params.tipo) {
+                case 'html':
+                    resolve(html);
+                    break;
+                case 'pdf':
+                default:
+                    pdf.create(html.toString(), this.options).toFile((err2, file) => {
+                        if (err2) {
+                            reject(err2);
+                        }
+                        resolve(file.filename);
+                    });
+                    break;
+            }
+        });
+    }
+
+    public async descargar(req, res, next) {
+        return new Promise(async (resolve, reject) => {
+            let file = await this.generar(req, next);
+            res.download(file, (err) => {
+                if (err) {
+                    reject(err);
                 }
+                resolve(true);
             });
         });
     }
--- modules/descargas/routes/descargas.ts
+++ modules/descargas/routes/descargas.ts
@@ -1,13 +1,21 @@
 import * as config from '../../../config';
 import * as express from 'express';
 import * as path from 'path';
+import { Auth } from '../../../auth/auth.class';
 import { Documento } from './../controller/descargas';
 
 let router = express.Router();
 
-
-router.post('/pdf', (req: any, res, next) => {
-    Documento.generarPDF(req, res, next);
+router.post('/:tipo', async (req: any, res, next) => {
+    if (!Auth.check(req, 'descargas:' + req.params.tipo)) {
+        return next(403);
+    }
+    try {
+        let doc = new Documento(req.body.options);
+        await doc.descargar(req, res, next);
+    } catch (e) {
+        return next(e);
+    }
 });
 
 export = router;
