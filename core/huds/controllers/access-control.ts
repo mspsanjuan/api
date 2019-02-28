2edd56cb37fa47f35f1bd91ed19ffb57889cc13a
LucasGonzalez <lucasgonzalezm3@gmail.com>
2019-02-28 16:00:37 -0300

    Permite a los profesionales ver la HUDS de pacientes en agendas futuras

--- core/huds/controllers/access-control.ts
+++ core/huds/controllers/access-control.ts
@@ -18,19 +18,20 @@ export async function hudsCheckPaciente(req: Express.Request, paciente: string):
     let check = false;
     if (paciente) {
         let pacienteBuscado = await pacienteModel.findById(paciente);
-        if (pacienteBuscado && (pacienteBuscado as any).documento === (req as any).user.usuario.documento) { // TODO: en algún momento la vinculación del usuario con el paciente o profesional se debe hacer de otra forma que con el documento
+        if (pacienteBuscado && (pacienteBuscado as any).documento.toString() === (req as any).user.usuario.documento.toString()) { // TODO: en algún momento la vinculación del usuario con el paciente o profesional se debe hacer de otra forma que con el documento
             check = true;
         } else {
             let profesional = await profesionalModel.findOne({ documento: (req as any).user.usuario.documento }, { _id: 1 });
             if (profesional) {
                 let agenda = await agendaModel.findOne({
                     'profesionales._id': (profesional as any)._id,
-                    'bloques.turnos': {
-                        $elemMatch: {
-                            'paciente.id': paciente,
-                            asistencia: 'asistio'
-                        }
-                    }
+                    // 'bloques.turnos': { // comentamos esto para considerar agendas futuras
+                    //     $elemMatch: {
+                    //         'paciente.id': paciente,
+                    //         asistencia: 'asistio'
+                    //     }
+                    // }
+                    'bloques.turnos.paciente.id': paciente
                 });
                 check = agenda ? true : false;
             }
@@ -58,14 +59,18 @@ export async function hudsCheckPaciente(req: Express.Request, paciente: string):
 export async function hudsCheckPrestaciones(req: Express.Request, prestaciones: any[]): Promise<boolean> {
     let check = true;
     if (prestaciones && prestaciones.length) {
-        // Obtiene pacientes únicos
-        let pacientes = [...new Set(prestaciones.filter(i => i.paciente).map(i => i.paciente.id.toString()))];
+        if (prestaciones.length === 1 && prestaciones[0].createdBy.username.toString() === (req as any).user.usuario.username.toString()) { // si es una sola prestación y fue creada por mí, tiene permiso. Es la primera vez que se atiende, no es paciente mío, pero debo poder ver la prestación que le estoy creando
+            check = true;
+        } else {
+            // Obtiene pacientes únicos
+            let pacientes = [...new Set(prestaciones.filter(i => i.paciente).map(i => i.paciente.id.toString()))];
 
-        // TODO: hacer las consultas más eficientes (múltiples a la vez?)
-        for (let i = 0; i < pacientes.length; i++) {
-            if (!await hudsCheckPaciente(req, pacientes[i])) {
-                check = false;
-                break;
+            // TODO: hacer las consultas más eficientes (múltiples a la vez?)
+            for (let i = 0; i < pacientes.length; i++) {
+                if (!await hudsCheckPaciente(req, pacientes[i])) {
+                    check = false;
+                    break;
+                }
             }
         }
     }
