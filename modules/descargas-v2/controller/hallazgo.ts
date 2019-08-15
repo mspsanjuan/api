4bc51b5196ecac3f82112cbb2e9269e834b9e6eb
nicodino <nicodinolfo@gmail.com>
2019-08-15 13:14:12 -0300

    refactor(RUP):Implementa archivo configuracion para descargas

--- modules/descargas-v2/controller/hallazgo.ts
+++ modules/descargas-v2/controller/hallazgo.ts
@@ -4,17 +4,6 @@ import { promisify } from 'util';
 import { join } from 'path';
 
 const read = promisify(readFile);
-const hallazgos = [
-    'hallazgo',
-    'situacion',
-    'trastorno',
-    'objeto físico',
-    'medicamento clínico',
-];
-
-export function esHallazgo(st) {
-    return hallazgos.findIndex(x => x === st) > -1;
-}
 
 export async function generarRegistroHallazgoHTML(hallazgo: any): Promise<any> {
     let template = await read(join(__dirname, '../../../templates/rup/informes/html/includes/hallazgo.html'), 'utf8');
